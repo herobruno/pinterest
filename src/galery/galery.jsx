cfd7b89bbce396b9622cd4f7b4711e95717ef3f5
herobruno <brunoss2118@gmail.com>
2025-05-05 14:04:51 -0300

    feat: terminando estilizaçãdo componente galery

--- src/galery/galery.jsx
+++ src/galery/galery.jsx
@@ -1,48 +1,111 @@
-import React from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
-import { Pagination } from 'swiper/modules';
+import React, { useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import { Pagination, Autoplay } from "swiper/modules";
+import "swiper/css";
+import "swiper/css/pagination";
 import "./galery.css";
-
-import 'swiper/css';
-import 'swiper/css/pagination';
 
 const Gallery = () => {
   const images = [
-    '/img/look1.jpg',
-    '/img/look2.jpg',
-    '/img/look3.jpg',
-    '/img/look4.jpg',
+    "./src/assets/img1.jpg",
+    "./src/assets/img2.jpg",
+    "./src/assets/img3.jpg",
+    "./src/assets/img4.jpg",
+    "./src/assets/img5.jpg",
+    "./src/assets/img6.jpg",
+    "./src/assets/img7.jpg",
+    "./src/assets/img8.jpg",
+    "./src/assets/img9.jpg",
+    "./src/assets/img10.jpg",
+    "./src/assets/img11.jpg",
+    "./src/assets/img12.jpg",
   ];
+
+  const subtitles = [
+    "ideia de look de verão",
+    "inspiração para seu estilo",
+    "tendência que combina com você",
+  ];
+
+  const subtitleColors = ["#2e7d32", "#1976d2", "#f57c00"];
+
+  const groupSize = 4;
+  const groupedImages = [];
+  for (let i = 0; i < images.length; i += groupSize) {
+    groupedImages.push(images.slice(i, i + groupSize));
+  }
+
+  const [activeIndex, setActiveIndex] = useState(0);
 
   return (
     <div className="gallery-container">
-    <h2 className="gallery-title">Encontre sua próxima</h2>
-    <h3 className="gallery-subtitle">ideia de look de verão</h3>
-
-    <Swiper
-      modules={[Pagination]}
-      pagination={{ clickable: true }}
-      spaceBetween={20}
-      slidesPerView={2}
-      breakpoints={{
-        640: { slidesPerView: 2 },
-        768: { slidesPerView: 3 },
-        1024: { slidesPerView: 4 },
-      }}
-      className="gallery-swiper"
-    >
-      {images.map((src, index) => (
-        <SwiperSlide key={index}>
-          <img
-            src={src}
-            alt={`Look ${index + 1}`}
-            className="gallery-image"
+      <h2 className="gallery-title">Encontre sua próxima</h2>
+      <h3
+        className="gallery-subtitle"
+        style={{ color: subtitleColors[activeIndex] }}
+      >
+        {subtitles[activeIndex]}
+      </h3>
+      <div className="custom-pagination">
+        {subtitles.map((_, index) => (
+          <span
+            key={index}
+            className={`custom-bullet ${activeIndex === index ? "active" : ""}`}
+            style={{
+              backgroundColor:
+                activeIndex === index ? subtitleColors[index] : "#ccc",
+            }}
           />
-        </SwiperSlide>
-      ))}
-    </Swiper>
-  </div>
-);
+        ))}
+      </div>
+
+      <Swiper
+        modules={[Pagination, Autoplay]}
+        autoplay={{ delay: 4000 }}
+        loop={true}
+        onAutoplay={(swiper) => {
+          const nextIndex = (swiper.realIndex + 1) % groupedImages.length;
+          setActiveIndex(nextIndex % subtitles.length);
+        }}
+        onSlideChange={(swiper) => {
+          setActiveIndex(swiper.realIndex % subtitles.length);
+        }}
+        className="gallery-swiper"
+      >
+        {groupedImages.map((group, index) => (
+          <SwiperSlide key={index}>
+            <div className="image-group">
+              <div className="flex flex-col justify-between">
+                <img
+                  src={group[0]}
+                  alt={`Look ${index}-0`}
+                  className="gallery-image translate-up"
+                />
+                <img
+                  src={group[1]}
+                  alt={`Look ${index}-1`}
+                  className="gallery-image translate-down"
+                />
+              </div>
+
+              <div className="flex flex-col justify-between items-end">
+                <img
+                  src={group[2]}
+                  alt={`Look ${index}-2`}
+                  className="gallery-image translate-up"
+                />
+                <img
+                  src={group[3]}
+                  alt={`Look ${index}-3`}
+                  className="gallery-image translate-down"
+                />
+              </div>
+            </div>
+          </SwiperSlide>
+        ))}
+      </Swiper>
+    </div>
+  );
 };
 
 export default Gallery;
