732d090a0683cc8b01d13ed8ea1426ac324cb157
Rodrigo <contato.rodrigoduartemkt@gmail.com>
2025-05-06 16:21:50 -0300

    chore: rodapé integrado ao componente principal da aplicação

--- src/components/app.tsx
+++ src/components/app.tsx
@@ -2,13 +2,17 @@ import React from "react";
 import Header from "../header/header";
 import Content from "../content/content";
 import Galery from "../galery/galery";
+import Rodape from '../footer/footer';
+import '../footer/footer.css';
 
 export default function App() {
   return (
     <div>
       <Header />
       <Content />
-      <Galery/>
+      <Galery />
+      <Rodape />
     </div>
   );
 }
+
